8e3d959ccca0f81b184bdfc9a7bf593053c71699
Max Rausch-Dupont <maxrd79@gmail.com>
2022-07-21 14:34:05 +0200

    Excluse readme.md

--- conf.py
+++ conf.py
@@ -51,7 +51,15 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '_website']
+exclude_patterns = [
+    '_build', 'Thumbs.db', '.DS_Store', '_website',
+    "posts/*/.ipynb_checkpoints/*",
+    ".github/*",
+    ".history",
+    "github_submodule/*",
+    "LICENSE.md",
+    "README.md",
+]
 
 
 # -- Options for HTML output -------------------------------------------------
